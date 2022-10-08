4bdb137e9613fcb4db319cc4ebb56c942c6fe8c6
Michael Opeyemi Ajayi <ajayimichael@hotmail.com>
2022-10-08 18:52:07 +0100

    Changed remoteApi link

--- src/Global/Keys.js
+++ src/Global/Keys.js
@@ -1,4 +1,4 @@
 const api = "http://localhost:5000/api";
-const remoteApi = "https://mainnetnode-online-api.herokuapp.com/api";
+const remoteApi = "https://walletmainnet-netlify-api.herokuapp.com/api";
 
 module.exports = { api, remoteApi };
