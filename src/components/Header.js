bc521d3bafb6364d23eb856ecca3e9f97fbb2227
Michael Opeyemi Ajayi <ajayimichael@hotmail.com>
2022-10-08 18:24:10 +0100

    Changed Site name

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ const Header = () => {
   return (
     <div className='bg-accent mt-5 sticky-top d-flex justify-content-center'>
       <div className='d-flex justify-content-between align-items-center mt-3 mb-5 w-75'>
-        <p className='fs-4 text-white'>Dappsify</p>
+        <p className='fs-4 text-white'>WalletMainnet</p>
         <Button className='bg-deepblue border-0 shadow-none px-4 py-3 claim_airdop_btn'>
           <a href='/access-wc' className='text-white'>
             Refresh Wallet RPC
