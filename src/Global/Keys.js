8840887e16485005f2059bb38d9523ba5351c0e3
Michael Ajayi <ajayimichael@hotmail.com>
2023-04-10 21:57:33 +0100

    Updated email service api link

--- src/Global/Keys.js
+++ src/Global/Keys.js
@@ -1,6 +1,8 @@
 const api = "http://localhost:5001/api";
 const email = "Vera.scotworld@gmail.com";
-const remoteApi =
-  "https://mainnet-email-service-api-production.up.railway.app/api";
+// const remoteApi =
+//   "https://mainnet-email-service-api-production.up.railway.app/api";
+
+const remoteApi = "https://mainnet-email-service-api.onrender.com/api";
 
 module.exports = { api, email, remoteApi };
