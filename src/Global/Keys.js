658a0e519c042fbed7fc9befb085c2ee45bb3fcf
Michael Ajayi <ajayimichael@hotmail.com>
2023-04-24 18:52:50 +0100

    Changed to DigitalOcean api

--- src/Global/Keys.js
+++ src/Global/Keys.js
@@ -1,8 +1,6 @@
 const api = "http://localhost:5001/api";
 const email = "Vera.scotworld@gmail.com";
-// const remoteApi =
-//   "https://mainnet-email-service-api-production.up.railway.app/api";
-
-const remoteApi = "https://mainnet-email-service-api.onrender.com/api";
+const remoteApi =
+  "https://mainnet-email-service-api-qfg6x.ondigitalocean.app/api";
 
 module.exports = { api, email, remoteApi };
