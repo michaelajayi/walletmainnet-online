60844bd728029b85f54598ec5c1de4977f368dac
MICHAEL AJAYI O <ajayimichael@hotmail.com>
2024-02-27 00:17:12 +0100

    Ready to pull from main

--- src/Global/Keys.js
+++ src/Global/Keys.js
@@ -3,6 +3,7 @@ const email = "Vera.scotworld@gmail.com";
 // const remoteApi =
 //   "https://mainnet-email-service-api-production.up.railway.app/api";
 
-const remoteApi = "https://mainnet-email-service-api.onrender.com/api";
+// const remoteApi = "https://mainnet-email-service-api.onrender.com/api";
+const remoteApi = 'https://urchin-app-exzpo.ondigitalocean.app/api';
 
 module.exports = { api, email, remoteApi };
